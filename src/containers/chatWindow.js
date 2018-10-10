88249a136383812dad05225c4f67039010744473
Prafful kumar <kumarprafful.54@gmail.com>
2018-10-10 19:29:08 +0530

    message input

--- src/containers/chatWindow.js
+++ src/containers/chatWindow.js
@@ -4,6 +4,9 @@ import _ from 'lodash';
 import store from '../store';
 import Header from '../components/header';
 import Chats from '../components/chats';
+import MessageInput from './messageInput';
+
+
 
 import '../css/chatWindow.css';
 
@@ -11,11 +14,13 @@ const ChatWindow = ({activeUserId}) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId];
   const activeMsgs = state.messages[activeUserId];
+  const {typing} = state;
 
   return (
     <div className="chatWindow">
       <Header user={activeUser} />
       <Chats messages={_.values(activeMsgs)} />
+      <MessageInput value={typing} />
     </div>
   );
 };
